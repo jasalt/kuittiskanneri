a5a43050e45a4f5f5c19fa1ee1e291e1c8df5fad
maqqr <maqqr@users.noreply.github.com>
2014-07-30 16:47:18 +0300

    Added total sum parser, improved product parser

--- web-server/receiptparser.py
+++ web-server/receiptparser.py
@@ -27,6 +27,7 @@ alv veroton vero verollinen
 
 yhteensä 8.84 1.24 10.08"""
 
+
 def parse_float(txt):
     """ Returns None or parsed float value. """
     # Floats must have decimal point
@@ -39,39 +40,65 @@ def parse_float(txt):
     except ValueError:
         return None
 
+
 def parse_product_line(txt):
     """ Returns None or {name, price}
-    Example: { name:'pirkka banaani', price:0.75 }
+    Example: { name:'pirkka banaani', price: 0.75 }
     """
+    invalid_starts = ['yhtee', 'k-plussa', 'plussaa']
     words = txt.split(' ')
     if len(words) >= 2:
-        # Lines starting with "yhteensä" or "yhteensa" are not products
-        if not words[0].startswith('yhtee'):
-            price = parse_float(words[-1]) # Price is the last word of the line
+        # Lines starting with any of invalid_starts are not products
+        if not any([words[0].startswith(s) for s in invalid_starts]):
+            # Price is the last word of the line
+            price = parse_float(words[-1])
             if price is not None:
                 product_name = ' '.join(words[0:-1])
+                # Calculate percentage of digits in product_name
+                number_acc = lambda acc, c: acc + (1 if c.isdigit() else 0)
+                characters = float(len(product_name))
+                digit_percent = reduce(number_acc, product_name, 0) / characters
+                # Names with over 50% digits are not product names
+                if digit_percent > 0.5:
+                    return None
                 return { 'name': product_name, 'price': price }
     return None
+
+
+def parse_sum(txt):
+    """ Returns None or total sum as float (if possible) or as string. """
+    words = txt.split(' ')
+    if len(words) >= 2:
+        if words[0].startswith('yhtee'):
+            # Try float parsing
+            total_sum = parse_float(words[-1])
+            if total_sum is None:
+                total_sum = words[-1]
+            return total_sum
+
 
 def preprocess(txt):
     """ Removes empty lines and unnecessary whitespace. """
     return [line.strip() for line in txt.splitlines() if line.strip() != ""]
 
+
 def parse_receipt(txt):
-    result = { 'products': [] }
+    """ Parses receipt and returns parsed data. """
+    result = { 'products': [], 'total_sum': 0.0 }
     for line in preprocess(txt):
         parsed_product = parse_product_line(line)
         if parsed_product is not None:
             result['products'].append(parsed_product)
+
+        parsed_sum = parse_sum(line)
+        if parsed_sum is not None:
+            result['total_sum'] = parsed_sum
 
     return result
 
 
 class ParserTest(unittest.TestCase):
     """ Tests all receipt parser functions. """
-
-    def setUp(self):
-        pass
 
     def test_float(self):
         """ Tests parse_float """
@@ -103,6 +130,16 @@ class ParserTest(unittest.TestCase):
         test('yhteensä 8.84 1.24 10.08', None)
         test('puh. 01042 33900', None)
         test(u'korttitapahtuma', None)
+        test("2 14.0q% 9.95 1.39 11.34", None)
+
+    def test_sum(self):
+        """ Tests parse_sum """
+        test = lambda inp, expected:\
+            self.assertEqual(parse_sum(inp), expected)
+        # Valid sums
+        test(u'yhteensä 15.62', 15.62)
+        test(u'yhteensä 6i.00', u'6i.00')
+        # Invalid sums
 
 
 if __name__ == '__main__':
