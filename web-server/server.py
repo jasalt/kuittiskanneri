b2747469db0a2a093451d405d55f7e33cfd707a4
ramdom <ramdom@server.fake>
2014-07-30 12:24:42 -0400

    Merged static file handlers into one function

--- web-server/server.py
+++ web-server/server.py
@@ -84,26 +84,9 @@ def ocr_testing(filename):
     '''
 
 
-@app.route('/js/<path:filename>')
-def web_static_js(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'js'), filename)
-
-@app.route('/css/<path:filename>')
-def web_static_css(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'css'), filename)
-
-@app.route('/fonts/<path:filename>')
-def web_static_fonts(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'fonts'), filename)
-
-@app.route('/img/<path:filename>')
-def web_static_img(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'img'), filename)
-
-@app.route('/partials/<path:filename>')
-def web_static_partials(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'partials'),
-        filename)
+@app.route('/static/<path:filename>')
+def web_static(filename):
+    return send_from_directory(STATIC_FOLDER, filename)
 
 
 @app.route('/')
@@ -113,3 +96,4 @@ def web_app():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8002, debug=True)
+
