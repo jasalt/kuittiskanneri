9dec2ae572f73e4c481e7b40bfe09aa8d1fe811e
maqqr <maqqr@users.noreply.github.com>
2014-07-30 20:56:45 +0300

    upload_file returns parsed receipt data

--- web-server/server.py
+++ web-server/server.py
@@ -24,6 +24,28 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+
+def optical_character_recognition(imagepath):
+    """ Does OCR on an image and returns tuple:
+        (raw text, autocorrected text, parsed receipt data) """
+    image_text = ""
+    proc = subprocess.Popen([OCR_SCRIPT, imagepath],
+                            stdout=subprocess.PIPE)
+    for line in iter(proc.stdout.readline, ''):
+        image_text += line.rstrip() + '\n'
+
+    image_text = image_text.decode('utf-8')
+
+    corrected_text = autocorrect.correct_text_block(image_text)
+
+    if corrected_text is unicode:
+        corrected_text = corrected_text.encode('utf-8')
+
+    return (image_text,
+            corrected_text,
+            receiptparser.parse_receipt(corrected_text))
+
+
 @app.route('/upload', methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
@@ -33,9 +55,11 @@ def upload_file():
             print("Save filename " + filename)
             if not os.path.exists(app.config['UPLOAD_FOLDER']):
                 os.mkdir(app.config['UPLOAD_FOLDER'])
-            image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            imagepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image_file.save(imagepath)
+            return optical_character_recognition(imagepath)[2]
+            #return redirect(url_for('uploaded_file',
+            #                        filename=filename))
     # GET
     return '''
     <!doctype html>
@@ -57,20 +81,8 @@ def ocr_testing(filename):
     if allowed_file(filename):
         imagepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         if os.path.exists(imagepath):
-            image_text = ""
-            proc = subprocess.Popen([OCR_SCRIPT, imagepath],
-                                    stdout=subprocess.PIPE)
-            for line in iter(proc.stdout.readline, ''):
-                image_text += line.rstrip() + '\n'
-
-            image_text = image_text.decode('utf-8')
-
-            corrected_text = autocorrect.correct_text_block(image_text)
-
-            if corrected_text is unicode:
-                corrected_text = corrected_text.encode('utf-8')
-
-            parse_result = receiptparser.parse_receipt(corrected_text)
+            (_, corrected_text, parse_result) =\
+                optical_character_recognition(imagepath)
 
             return '''
             <!doctype html>
