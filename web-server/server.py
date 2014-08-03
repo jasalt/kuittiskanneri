2c04dbde260ded893c01abe22fc680ceee8bcde8
ramdom <undeadj@gmail.com>
2014-08-03 07:18:53 -0400

    Added ImageMagic preprocessing

--- web-server/server.py
+++ web-server/server.py
@@ -29,8 +29,16 @@ def allowed_file(filename):
 def optical_character_recognition(imagepath):
     """ Does OCR on an image and returns tuple:
         (raw text, autocorrected text, parsed receipt data) """
+    
+    # Process image with ImageMagick
+    tempimagepath = os.path.join(app.config['UPLOAD_FOLDER'], 'temp.png')
+    im_proc = subprocess.Popen(['convert',imagepath,'-resize','600x800',
+                                '-blur','2','-lat','8x8-2%',tempimagepath],
+                                stdout=subprocess.PIPE)
+    im_proc.communicate()
+
     image_text = ""
-    proc = subprocess.Popen([OCR_SCRIPT, imagepath],
+    proc = subprocess.Popen([OCR_SCRIPT, tempimagepath],
                             stdout=subprocess.PIPE)
     for line in iter(proc.stdout.readline, ''):
         image_text += line.rstrip() + '\n'
