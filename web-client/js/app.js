0430e92e5b2bff7781b0cfe796e3c5abad4ceff0
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-08-28 10:54:29 +0300

    Add ngCookies. Create separate registering page.

--- web-client/js/app.js
+++ web-client/js/app.js
@@ -1,16 +1,32 @@
+var login = angular.module('myLoginCheck',[]).
+factory('$logincheck', function(){
+  return function(userid){
+  //Perform logical user loging check either by looking at cookies or make a call to server
+  if(userid > 0) return true; //TODO
+  return false;
+  };
+});
+
+
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
     'ngRoute',
+    'ngCookies',
     'myApp.filters',
     'myApp.services',
     'myApp.directives',
-    'myApp.controllers'
+    'myApp.controllers',
+    'myLoginCheck'
 ]).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: 'IndexCtrl'});
+        $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
         $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
         $routeProvider.when('/upload', {templateUrl: 'partials/upload.html', controller: 'UploadCtrl'});
         $routeProvider.when('/receipt', {templateUrl: 'partials/receipt.html', controller: 'ReceiptCtrl'});
         $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'});
         $routeProvider.otherwise({redirectTo: '/'});
-    }]);
+    }])
+    .run(function($logincheck, $location) {
+        console.log("User logged in: ", $logincheck(5));
+    });
