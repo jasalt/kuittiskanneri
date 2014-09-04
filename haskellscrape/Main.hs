6c7d5d11054b058049a9372fc443476c2079a998
maqqr <maqqr@users.noreply.github.com>
2014-09-04 18:40:51 +0300

    Haskell scraper done, scandinavic characters are broken

--- haskellscrape/Main.hs
+++ haskellscrape/Main.hs
@@ -1,37 +1,57 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
--- Vaatii TagSoup moduulin
--- cabal install tagsoup
-
-import Data.List (isPrefixOf)
-import Data.Maybe (fromMaybe)
+import Data.List
+import Data.Text.Encoding
+import qualified Data.Text as T
+import qualified Data.ByteString as BS
 import Network.HTTP
 import Control.Monad
 import Text.HTML.TagSoup
 
--- | Avaa urlin ja kaivaa vastauksesta sisällön
+data Hole = Hole
+hole :: Hole
+hole = undefined
+
+-- | Avaa urlin ja kaivaa vastauksesta sisällön.
 openUrl :: String -> IO String
 openUrl url = simpleHTTP (getRequest url) >>= getResponseBody
 
+
 -- | Kerää nettisivulta kaikki linkit, joiden href alkaa
---   merkkijonolla "tuotteet"
-scrapeProductLinksHrefs :: String -> IO [String]
-scrapeProductLinksHrefs = liftM (map getHref . filter productLink) . fmap parseTags . openUrl
+--   merkkijonolla "tuotteet".
+scrapeCategoryLinkHrefs :: String -- ^ Kategoriasivun URL
+                        -> IO [String]
+scrapeCategoryLinkHrefs = liftM (map getHref . filter productLink) . fmap parseTags . openUrl
     where
         -- Ottaa tagista hrefin arvon talteen
         getHref :: Tag String -> String
-        getHref (TagOpen "a" attr) = fromMaybe "" $ lookup "href" attr
+        getHref tag
+            | isTagOpen tag = fromAttrib "href" tag
+            | otherwise     = ""
 
         -- Palauttaa True, jos tagi on linkki, ja sen hrefin alussa on "tuotteet"
         productLink :: Tag String -> Bool
         productLink (TagOpen "a" attr) = any (\(_, v) -> "tuotteet" `isPrefixOf` v) attr
-        productLink _                        = False
+        productLink _                  = False
 
+
+-- | Kerää tuotesivulta tuotteiden nimet.
+scrapeProductNames :: String -- ^ Tuotesivun URL
+                   -> IO [String]
+scrapeProductNames productPageUrl = do
+    putStrLn $ "Scraping " ++ productPageUrl
+    tags <- fmap parseTags $ openUrl productPageUrl
+    let productTags = sections (~== "<div class=product-text>") tags
+    return $ map (fromTagText . flip (!!) 3) productTags
+
+
+-- | Kerää tuotteet scrape.txt tiedostoon.
 main :: IO ()
 main = do
-  categoryUrls <- scrapeProductLinksHrefs testUrl
-  return ()
+    categoryUrls <- (fmap . map) (baseUrl ++) (scrapeCategoryLinkHrefs testUrl)
+    productNames <- fmap concat $ mapM scrapeProductNames categoryUrls
+    BS.writeFile "scrape.txt" . encodeUtf8 . T.pack $ intercalate "\n" productNames
     where
         testUrl =  "http://www.rainbow.fi/rainbow-tuotteet/selaa-tuotteita/"
-        testUrl2 = "http://www.rainbow.fi/tuotteet/elintarvikkeet/hedelmat-ja-marjat-tuore/"
-
+        baseUrl = "http://www.rainbow.fi/"
