0192f7438d679c02639082f7be2ff96f94e06c5d
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-09-04 15:27:37 +0300

    Receipt viewing partial without editing.

--- web-client/js/services.js
+++ web-client/js/services.js
@@ -7,8 +7,6 @@ app.service('receiptService', function($http, $location) {
     /*
      * Provide receipt data between different controllers.
      */
-    this.mockReceipt = {"date":{"Y":2014,"h":23,"m":15,"M":7,"D":16},"total_sum":null,"shop_name":"abc heinola","products":[{"price":2.39,"name":"hedelmäpupmijggurtti"},{"price":0.34,"name":"hakarueji"},{"price":7.29,"name":"hyvä suomi arkijuusto"},{"price":2.45,"name":"maksalaatikko"},{"price":2.49,"name":"broileripyörykät"},{"price":0.21,"name":"muovikassi 40l valio"},{"price":5.35,"name":"vmssezsn"}],"credit_card":true};
-
     var receipt = null;
 
     var userReceipts = null;
