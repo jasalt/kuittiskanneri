4157fe7f33c14752acb2e35153d9ba3fd5547425
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-09-14 12:07:19 +0300

    Simplify serving static files (for development)

--- app/modules/serve_static.py
+++ app/modules/serve_static.py
@@ -12,47 +12,12 @@ mod = Blueprint('serve_static', __name__)
 from app import app
 
 
-@mod.route('/test2')
-def show():
-    return "Helloworld"
-
-
-STATIC_FOLDER = app.static_folder
-
-
 @mod.route('/')
-def web_app():
-    print("Serve static app")
-
-    return send_from_directory(STATIC_FOLDER, 'index.html')
+def root():
+    return app.send_static_file('index.html')
 
 
-@mod.route('js/<path:filename>')
-def web_static_js(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'js/'), filename)
-
-
-@mod.route('css/<path:filename>')
-def web_static_css(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'css'), filename)
-
-
-@mod.route('fonts/<path:filename>')
-def web_static_fonts(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'fonts'), filename)
-
-
-@mod.route('img/<path:filename>')
-def web_static_img(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'img'), filename)
-
-
-@mod.route('partials/<path:filename>')
-def web_static_partials(filename):
-    return send_from_directory(os.path.join(STATIC_FOLDER, 'partials'),
-                               filename)
-
-
-# @mod.route('static/<path:filename>')
-# def web_static(filename):
-#     return send_from_directory(os.path.jount(STATIC_FOLDER, filename))
+@app.route('/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
