1432dff2beb96ab64dd29b4e997ca60c59898dcd
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-09-16 12:49:50 +0300

    Add receipt manually. Add/rm product fields.

--- app/static/angular/receipt/receipt-controller.js
+++ app/static/angular/receipt/receipt-controller.js
@@ -1,19 +1,39 @@
 angular.module('myApp.receipt', ['myApp.userAuthentication', 'myApp.receiptService'])
-    .controller('ReceiptController', function($scope, $location, receiptService) {
-        // TODO DEV MOCk get the real receipt
+    .controller('ReceiptController', function($scope, $location, receiptService, userService) {
         $scope.origReceipt = receiptService.getReceipt();
-        debugger;
-        if ("_id" in $scope.origReceipt){
-            // The receipt is new so start editing.
-            console.log("receipt has id");
-            console.log($scope.origReceipt['_id']);
-            $scope.editingReceipt = false;
+
+        $scope.editingReceipt = true;
+
+        if ($scope.origReceipt){
+            if ("_id" in $scope.origReceipt){
+                // The receipt is new so start editing.
+                console.log("Editing a stored receipt");
+                console.log($scope.origReceipt['_id']);
+                $scope.editingReceipt = false;
+            }
+            console.log("Editing unsaved receipt");
         } else {
-            console.log("receipt has not id ");
-            $scope.editingReceipt = true;
+            console.log("Creating a new receipt");
+            $scope.origReceipt = { "products": [],
+                                   credit_card: false,
+                                   total_sum: null,
+                                   shop_name: null,
+                                   user: userService.getUsername(),
+                                   date: "today" // TODO set this date
+                                   // TODO datepicker
+                                 };
         }
 
         $scope.receipt = $scope.origReceipt;
+
+        $scope.addProductField = function() {
+            $scope.receipt["products"].push({});
+
+        };
+
+        $scope.removeProductField = function(product) {
+            $scope.receipt["products"] = _.without($scope.receipt.products, product);
+        };
 
         $scope.changePaymentType = function() {
             $scope.receipt.credit_card = !$scope.receipt.credit_card;
